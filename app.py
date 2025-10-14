fccb9882278dece8b31fce7ce6d0593c4f2c62ca
minelyeu <versizeudm123@gmail.com>
2025-10-14 18:59:24 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, session
 import random
 import string
 
 app = Flask(__name__)
+app.secret_key='supersecretkey'
+
+USERS = [
+    {'login': "admin", "password": "admin"},
+    {'login': 'user', 'password': 'qwerty'}    
+]
+
+def checkl(login,password):
+    for user in USERS:
+        if user['login'] == login and user['password'] == password:
+            return True
+    return False
 
 def generate_password(length, complexity):
     if complexity == "low":
@@ -18,23 +30,42 @@ def generate_password(length, complexity):
     return password
 
 @app.route('/', methods=['GET', 'POST'])
-def index():
-    passwords = []
+def login():
+    if request.method == 'POST':
+        login = request.form['login']
+        password = request.form['password']
+        if checkl(login, password):
+            session['user'] = login
+            return redirect(url_for('index'))
+        else:
+            return render_template('login.html', error='Неверный логин или пароль')
+    return render_template('login.html')
 
+@app.route('/generator', methods=['GET', 'POST'])
+def index():
+    if 'user' not in session:
+        return redirect(url_for('login'))
+
+    passwords = []
     if request.method == 'POST':
         try:
             length = int(request.form['length'])
             count = int(request.form['count'])
             complexity = request.form['complexity']
-
-            for _ in range(count):
-                pwd = generate_password(length, complexity)
-                passwords.append(pwd)
-
+            with open("password_history.txt", "a", encoding="utf-8") as f:
+                for _ in range(count):
+                    pwd = generate_password(length, complexity)
+                    passwords.append(pwd)
+                    f.write(f"{session['user']} | {pwd}\n")
         except:
             passwords = ["Ошибка ввода. Попробуйте ещё раз."]
 
-    return render_template('index.html', passwords=passwords)
+    return render_template('index.html', passwords=passwords, user=session['user'])
+
+@app.route('/logout')
+def logout():
+    session.pop('user', None)
+    return redirect(url_for('login'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=7000)
